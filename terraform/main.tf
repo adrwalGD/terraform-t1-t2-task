d18df15812fd102838960e6f0b67519526085ad5
xDepcio <olek.drwal@gmail.com>
2025-05-12 13:37:21 +0200

    setting up storage ac and vault

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,3 +38,110 @@ resource "azurerm_subnet" "main_subnet" {
   virtual_network_name = azurerm_virtual_network.main_vnet.name
   address_prefixes     = ["10.0.0.0/24"]
 }
+
+# =================== blob storage ===================
+resource "azurerm_storage_account" "main_storage" {
+  name                     = "adrwalstorageacblob"
+  resource_group_name      = azurerm_resource_group.main_rg.name
+  location                 = azurerm_resource_group.main_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "main_container" {
+  name                  = "adrwalcontainer"
+  storage_account_id    = azurerm_storage_account.main_storage.id
+  container_access_type = "private"
+}
+
+resource "azurerm_private_endpoint" "sotrage_acc_endpoint" {
+  name                = "adrwal-storage-endpoint"
+  location            = azurerm_resource_group.main_rg.location
+  resource_group_name = azurerm_resource_group.main_rg.name
+
+  subnet_id = azurerm_subnet.main_subnet.id
+
+  private_service_connection {
+    name                           = "adrwal-storage-connection"
+    is_manual_connection           = false
+    private_connection_resource_id = azurerm_storage_account.main_storage.id
+    subresource_names              = ["blob"]
+  }
+
+  private_dns_zone_group {
+    name = "adrwal-storage-dns-group"
+    private_dns_zone_ids = [
+      azurerm_private_dns_zone.storage_acc_dns_zone.id,
+    ]
+  }
+}
+
+resource "azurerm_private_dns_zone" "storage_acc_dns_zone" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = azurerm_resource_group.main_rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "vnet_storage_acc" {
+  name                  = "adrwal-storage-vnet-link"
+  resource_group_name   = azurerm_resource_group.main_rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.storage_acc_dns_zone.name
+  virtual_network_id    = azurerm_virtual_network.main_vnet.id
+  registration_enabled  = false
+}
+# ==================================================
+
+# =================== vault ===================
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "vault" {
+  name                          = "adrwalvault"
+  location                      = azurerm_resource_group.main_rg.location
+  resource_group_name           = azurerm_resource_group.main_rg.name
+  tenant_id                     = data.azurerm_client_config.current.tenant_id
+  sku_name                      = "standard"
+  public_network_access_enabled = false
+}
+
+resource "azurerm_key_vault_access_policy" "vault_access" {
+  key_vault_id = azurerm_key_vault.vault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  key_permissions    = ["Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore", "Decrypt", "Encrypt", "UnwrapKey", "WrapKey", "Verify", "Sign", "Purge", "Release", "Rotate", "GetRotationPolicy", "SetRotationPolicy"]
+  secret_permissions = ["Get", "List", "Set", "Delete", "Recover", "Backup", "Restore", "Purge"]
+}
+
+resource "azurerm_private_endpoint" "vault_pe" {
+  name                = "adrwal-vault-endpoint"
+  location            = azurerm_resource_group.main_rg.location
+  resource_group_name = azurerm_resource_group.main_rg.name
+
+  subnet_id = azurerm_subnet.main_subnet.id
+
+  private_service_connection {
+    name                           = "adrwal-vault-connection"
+    is_manual_connection           = false
+    private_connection_resource_id = azurerm_key_vault.vault.id
+    subresource_names              = ["vault"]
+  }
+
+  private_dns_zone_group {
+    name                 = "adrwal-vault-dns-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.vault_dns_zone.id]
+  }
+}
+
+resource "azurerm_private_dns_zone" "vault_dns_zone" {
+  name                = "privatelink.vault.azure.net"
+  resource_group_name = azurerm_resource_group.main_rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "vnet_vault" {
+  name                  = "adrwal-vault-vnet-link"
+  resource_group_name   = azurerm_resource_group.main_rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.vault_dns_zone.name
+  virtual_network_id    = azurerm_virtual_network.main_vnet.id
+  registration_enabled  = false
+}
+# ==================================================
